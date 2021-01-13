8c58599c87461743c0ddeb71dc0e1cf7d8a4a38e
Mezix <mezteenx@googlemail.com>
2021-01-13 18:52:57 +0100

    first commit

---
+++ src/Server.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.net.*;
+
+public class Server {
+
+    public static void main (String[] args) throws IOException
+    {
+        ServerSocket serverSocket = null;
+        int port = 5000;
+
+        try
+        {
+            serverSocket = new ServerSocket(port);
+        }
+        catch (IOException e)
+        {
+            System.err.println("Could not listen on port:" + port);
+            System.exit(1);
+        }
+
+        Socket clientSocket = null;
+        PrintWriter out = null;
+        BufferedReader in = null;
+
+        while(true)
+        {
+            try
+            {
+                clientSocket = serverSocket.accept();
+                out = new PrintWriter(clientSocket.getOutputStream(), true);
+                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            }
+            catch (IOException e)
+            {
+                System.err.println("Accept failed");
+                break;
+            }
+            String input;
+            while ((input = in.readLine()) != null)
+            {
+                System.out.println("Received: " + input);
+                if(input.equals("Bye"))
+                    break;
+                out.println("Im a dummy");
+            }
+            out.println("Bye");
+
+            out.close();
+            in.close();
+            clientSocket.close();
+        }
+        serverSocket.close();
+    }
+}
